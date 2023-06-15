b0e44111eeeb7601b5804c32fd8a5982b6a60b41
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-15 15:00:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcomr is a place of art,\n");
+
+	return (0);
+}
