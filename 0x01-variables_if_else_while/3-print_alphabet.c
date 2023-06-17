a9a85937152b568a8222d1bbfc46baa590298eeb
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-17 15:46:59 -0700

    Print Aphabet-3

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This the entry point
+ *
+ * Return: return 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z';)
+	{
+		putchar(ch);
+		(ch++);
+	}
+
+	for (ch = 'A'; ch <= 'Z';)
+	{
+		putchar(ch);
+		(ch++);
+	}
+	putchar('\n');
+
+	return (0);
+}
