db4162fa3b0b5fec35e5a9888c1c59fbfa18d9c5
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-18 13:08:03 -0700

    Prinr Comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - This the entry point
+ *
+ * Return: return 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 8; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (n != 8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
