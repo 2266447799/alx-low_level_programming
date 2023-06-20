1b9f9586a32163df4312fda204b9ad1802e4ebf2
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-20 15:58:43 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: character to be checked
+ *
+ * Return: return integer
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
