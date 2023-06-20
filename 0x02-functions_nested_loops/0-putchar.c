0bb4124886410efafb40290e78f5d22476e59fca
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-20 04:38:13 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character p to stdout
+ * @p: The character to print
+ *
+ * Return: On Success 1.
+ * On error, -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char p)
+{
+	return (write(1, &p, 1));
+}
+
+/**
+ * main - This the entry point
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
