e69def3276124073529438902de18e7551663c0a
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-21 14:08:05 -0700

    102-fibonnaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,32 +3,29 @@
 /**
  * main - prints the first 50 Fibonacci numbers
  *
- * Return: returns 0.
+ * Return: return 0.
  */
 
 int main(void)
 {
-	int i, j, k, next;
+	long int i, j, k, next;
 
 	j = 1;
-
 	k = 2;
 
-	for (i = 1; i <= 50; i++)
+	for (i = 0; i <= 50; i++)
 	{
 		if (j != 20365011074)
 		{
-			printf("%d, ", j);
+			printf("%ld, ", j);
 		}
 		else
 		{
-			printf("%d\n", j);
+			printf("%ld\n", j);
 		}
 		next = j + k;
 		j = k;
 		k = next;
 	}
-	printf("\n");
-
 	return (0);
 }
