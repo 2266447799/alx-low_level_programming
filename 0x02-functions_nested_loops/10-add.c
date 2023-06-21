1cf0ea000ff1cf08d828fbea7ce5668abe67e2cf
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-20 17:43:49 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: first integer
+ * @j: second integer
+ *
+ * Return: returns n
+ */
+
+int add(int i, int j)
+{
+	int m;
+
+	m = i + j;
+
+	return (m);
+}
