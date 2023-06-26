33a9916d02b99c3d49eb91146e8c8f30a5b52167
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-06-26 11:48:00 -0700

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string into integer
+ * @s: the string to be converted
+ *
+ * Return: string to be converted
+ */
+
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * i);
+}
