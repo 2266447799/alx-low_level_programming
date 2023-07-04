10b936c3613a6e55e18e04175417e83b900914c6
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-07-03 17:07:57 -0700

    Strchr4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
+	unsigned int i = 0;
 
-		s++;
-	}
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
 	return ('\0');
 }
