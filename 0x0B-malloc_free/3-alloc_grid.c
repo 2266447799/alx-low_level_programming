1503da7e4645767c4c6c29feed1ea4cb38fc6c49
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-07-11 16:49:15 -0700

    Alloc grid correction

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,11 +29,11 @@ int **alloc_grid(int width, int height)
 			for (i--; 1 > 0; i--)
 				free(grid[i]);
 
-			free(grid);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
 	return (grid);
+	free(grid);
 }
