a83f81acb543c93e864f2f041659d7016295f42b
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-07-11 17:34:08 -0700

    alloc grid correction 2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(**grid));
+		grid[i] = (int *)malloc(width * sizeof(**grid));
 		if (grid[i] == NULL)
 		{
 			for (i--; 1 > 0; i--)
