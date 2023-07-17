a1483fe70e06161c8a7d190d7cb7520f00321fea
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-07-17 13:57:19 -0700

    Sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Description: function-like maacro that computes the sum of numbers
+ * @x: first number
+ * @y: second number
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
