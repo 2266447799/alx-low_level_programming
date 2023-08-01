2f36f8184ff8593d0645b7af7e2af4c9288dc16a
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-08-01 16:15:21 -0700

    Delete head node correction

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,15 +10,14 @@
 int pop_listint(listint_t **head)
 {
 	int data;
-	listint_t *temp, *ptr;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return (0);
 
-	ptr = *head;
-	data = ptr->n;
-	temp = ptr;
-	ptr = ptr->next;
+	data = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
 	free(temp);
 
 	return (data);
