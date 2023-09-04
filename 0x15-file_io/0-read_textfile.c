dd7d8eadb626c02dd80befdd7ce350c98f4eb38b
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-09-04 15:57:14 -0700

    Correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,37 +14,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fi;
-	ssize_t re, wr;
-	char *buffer = malloc(letters);
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
-	if (!filename || !buffer)
+	if (filename == NULL)
 		return (0);
 
-	fi = open(filename, O_RDONLY);
-	if (fi == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	re = read(fi, buffer, letters);
-	if (re == -1)
-		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		close(fi);
-		free(buffer);
+		close(fd);
 		return (0);
 	}
 
-	buffer[letters] = '\0';
-
-	wr = write(STDOUT_FILENO, buffer, re);
-	if (wr == -1)
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
-		close(fi);
 		free(buffer);
 		return (0);
 	}
-
-	close(fi);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	return (wr);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
