5bd6df04998f8f86ff71d4c171c7b624b89f320b
Sanelisiwe Williams <sanelisiwewilliams1@gmail.com>
2023-09-04 16:16:23 -0700

    Correction 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fi = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0060);
+	fi = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fi == -1)
 		return (-1);
 
